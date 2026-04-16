a452182f8f71a21ad50baba0aa72421cfb3ca1f6
kapufskipauw <rosarhode@gmail.com>
2026-04-16 10:19:59 +0200

    refactor: Migrated routes to IdeaController

--- routes/web.php
+++ routes/web.php
@@ -1,60 +1,15 @@
 <?php
 
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Idea;
 
-
- // index
-Route::get('/ideas', function () {
-    $ideas = Idea::all();
-
-    return view('ideas.index', [
-        'ideas' => $ideas,
-    ]);
-});
-
-
-// show
-Route::get('/ideas/{idea}', function (Idea $idea) {
-
-    return view('ideas.show', [
-        'idea' => $idea,
-    ]);
-});
-
-//edit
-Route::get('/ideas/{idea}/edit', function (Idea $idea) {
-
-    return view('ideas.edit', [
-        'idea' => $idea,
-    ]);
-});
-
-//update
-Route::patch('/ideas/{idea}', function (Idea $idea) {
-   $idea->update([
-        'description' => request('description'),
-    ]);
-
-    return redirect("/ideas/$idea->id");
-});
-
-// store
-Route::post('/ideas', function () {
-    Idea::create([
-        'description' => request('description'),
-        'state' => 'pending',
-    ]);
-
-    return redirect('/ideas');
-});
-
-//destroy
-Route::delete('/ideas/{idea}', function (Idea $idea) {
-    $idea->delete();
-
-    return redirect('/ideas');
-});
+Route::get('/ideas', [IdeaController::class, 'index']);
+Route::get('/ideas/create', [IdeaController::class, 'create']);
+Route::post('/ideas', [IdeaController::class, 'store']);
+Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
+Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
+Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
 
 
 

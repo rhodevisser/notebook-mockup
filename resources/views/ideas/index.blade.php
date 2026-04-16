351076ea3742e72ee32e36c2042a3cb7e6c648ac
kapufskipauw <rosarhode@gmail.com>
2026-04-16 10:18:20 +0200

    build: Added create.blade.php
    So create form is separated and if there are no idea in show yet there is a link to create and idea.

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -1,35 +1,10 @@
 <x-layout>
 
-    <form
-        method="POST"
-        action="/ideas">
-        @csrf
-
-        <div class="col-span-full">
-            <label for="description"
-                   class="block text-sm/6 font-medium text-white">
-                New Idea
-            </label>
-            <div class="mt-2">
-                <textarea id="description"
-                          name="description"
-                          rows="3"
-                          class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
-            </div>
-            <p class="mt-3 text-sm/6 text-gray-400">Have an idea you want to save for later?</p>
-        </div>
-
-        <div class="mt-6 flex items-center gap-x-6">
-            <button
-                type="submit"
-                class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-                Save
-            </button>
-        </div>
-
         @if($ideas->count())
             <div class="mt-6 text-white">
-                <h2 class="font-bold ">Your ideas: </h2>
+                <h2 class="font-bold ">
+                    Your ideas:
+                </h2>
 
                 <ul class="mt-5">
                     @foreach($ideas as $idea)
@@ -43,11 +18,15 @@
                     @endforeach
                 </ul>
             </div>
+            @else
+            <p>
+                You have no ideas yet!
+                <a
+                    href="/ideas/create"
+                    class="text-blue-500 underline">
+                    Create your idea here...
+                </a>
+            </p>
         @endif
-
-
-
-
-    </form>
 
 </x-layout>

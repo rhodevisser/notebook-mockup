1a0c12140f2006e8ea105eb18888a7627272842a
kapufskipauw <rosarhode@gmail.com>
2026-04-27 18:25:51 +0200

    fix: Issue with closing tag

--- resources/views/components/idea-card.blade.php
+++ resources/views/components/idea-card.blade.php
@@ -1,4 +1,4 @@
-    <a {{$attributes->merge(['class' => 'card bg-neutral text-neutral-content w-96']) }}">
+    <a {{$attributes->merge(['class' => 'card bg-neutral text-neutral-content w-96']) }}>
         <div class="card-body">
             <h2 class="card-title">
                 {{ $slot }}

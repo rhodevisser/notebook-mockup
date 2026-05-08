c5c12ae666de52ccec96edfe38465f6502a8e331
kapufskipauw <rosarhode@gmail.com>
2026-05-08 15:03:32 +0200

    refactor: Installed Vite to be locally

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,16 +4,14 @@
 
 
 <!DOCTYPE html>
-<html lang="nl" data-theme="synthwave">
+<html lang="nl"
+      data-theme="synthwave">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
